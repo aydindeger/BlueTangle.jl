93c6b32221c3e3c4d128955584ad0f0f35a925f5
aydindeger <aydindeger@icloud.com>
2024-05-16 15:28:26 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,13 @@ using Test
 
 @testset "BlueTangle.jl" begin
     @test BlueTangle.int2bin(2,4)==[0,0,1,0]
+
+    @test begin #this checks nonlocal hilbert construction
+        N=6
+        qubit=2
+        control=5
+        a=Op("CNOT",qubit,control).expand(N)
+        b=Op("X",control;control=qubit).expand(N)
+        a==b
+    end
 end
