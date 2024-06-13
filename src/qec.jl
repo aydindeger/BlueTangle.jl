a186f0c1385828c822fd0377c40d1081537bc7a2
aydindeger <aydindeger@icloud.com>
2024-06-13 23:13:20 +0100

    feat: logical X,Z implemented

--- src/qec.jl
+++ src/qec.jl
@@ -1,5 +1,5 @@
 """
-_get_codeword
+get_codeword(codestate::sa.SparseVector)
 """
 get_codeword(codestate::sa.SparseVector)=fock_basis.(findall(x->!isapprox(x,0;atol=1000eps()),codestate).-1,get_N(codestate))
 
@@ -30,18 +30,17 @@ end
 """
 standard_form_logicals(G::AbstractMatrix)
 """
-function standard_form_logicals(G::AbstractMatrix)
+function standard_form_logicals(G::AbstractMatrix,logical_XZ_ops::Dict)
     n, m = size(G, 2) ÷ 2, size(G, 1)
     k = n - m
     
     generator = G[:, 1:n]
     G2 = G[:, (n+1):end]
-    
-    # Step 1: Gaussian Elimination on generator & adjust G2 rows & cols accordingly
+
     generator_rref, r, generator_transform_rows, generator_transform_cols = reduced_row_echelon(generator)
     G = hcat(generator_rref, mod.(generator_transform_rows * G2 * generator_transform_cols, 2))
     
-    # Step 2: Gaussian Elimination on E
+    # Gaussian Elimination on E
     A = G[1:r, (r+1):n]
     B = G[1:r, n+1:(n+r)]
     C = G[1:r, (n+r+1):end]
@@ -62,11 +61,18 @@ function standard_form_logicals(G::AbstractMatrix)
 
     id, A1, A2, B, C1, C2, D, E2 = _extract_blocks(G_standard,r)
 
-    logical_Z_vec=[fill(0,n)...,A2'...,fill(0,m-r)...,fill(1,k)...]
-    logical_X_vec=[fill(0,r)...,E2'...,fill(1,k)...,E2'C1'+C2'...,fill(0,m-r)...,fill(0,k)...]
+    logical_XZ_vecs=Dict()
 
-    return G_standard, logical_vec_2_ops.([logical_X_vec,logical_Z_vec]), (logical_X_vec,logical_Z_vec)
+    for ki=1:k
+        logical_XZ_vecs["logZ",ki]=[fill(0,n)...,A2'[ki,:]...,fill(0,m-r)...,fill(1,k)...]
+        logical_XZ_vecs["logX",ki]=[fill(0,r)...,E2'[ki,:]...,fill(1,k)...,((E2' * C1') + C2')[ki,:]...,fill(0,m-r)...,fill(0,k)...]
+        logical_XZ_ops["logZ",ki]=logical_vec_2_ops(logical_XZ_vecs["logZ",ki])
+        logical_XZ_ops["logX",ki]=logical_vec_2_ops(logical_XZ_vecs["logX",ki])
+    end
+
+    return G_standard, logical_XZ_ops, logical_XZ_vecs, r
 end
+
 
 function logical_vec_2_ops(logical_vec::Vector)
 
@@ -76,10 +82,10 @@ function logical_vec_2_ops(logical_vec::Vector)
     for (i,x)=enumerate(logical_vec)
         if x==1
             if i<=n
-                o=logical_vec[i+n] == 1 ? Op("Y",i) : Op("X",i)
+                o=logical_vec[i+n] == 1 ? ("Y",i) : ("X",i)
                 push!(ops,o)
             elseif i>n
-                o=logical_vec[i-n] == 1 ? Op("Y",i-n) : Op("Z",i-n)
+                o=logical_vec[i-n] == 1 ? ("Y",i-n) : ("Z",i-n)
                 push!(ops,o)
             end
         end
@@ -89,10 +95,8 @@ function logical_vec_2_ops(logical_vec::Vector)
     end
 
 
-
-function _extract_blocks(G_standard_form,r::Int)
+function _extract_blocks(G_standard_form,r::Int) #r=rank
     #block sizes: r, m-r, k
-    # r=la.rank(G);println("rank of G=$(r)")
     n, m = size(G_standard_form, 2) ÷ 2, size(G_standard_form, 1)
     k = n - m
 
@@ -133,6 +137,47 @@ function _extract_blocks(G_standard_form,r::Int)
     return id, A1, A2, B, C1, C2, D, E2
 end
 
+function encoding_circuit_from_generator(generator_standard::AbstractMatrix,logical_XZ_vecs::Dict,r::Int)
+
+    #stac ebook, gottesman's thesis
+    m,n=size(generator_standard,1),size(generator_standard,2) ÷ 2
+    k=n-m
+
+    standard_generators_x=generator_standard[:,1:n]
+    standard_generators_z=generator_standard[:,n+1:end]
+
+    encoding_circuit = Vector{Op}()
+
+    # First loop: Add CX gates based on logical_xs matrix
+    for i in 1:k
+        for j in r+1:n-k
+            if logical_XZ_vecs["logX",i][j]==1
+                push!(encoding_circuit, Op("CX", n-k+i, j))
+            end
+        end
+    end
+
+    # Second loop: Add H, CX, and CZ gates based on standard_generators_x and standard_generators_z matrices
+    for i in 1:r
+        push!(encoding_circuit, Op("H", i))
+        for j in 1:n
+            if i == j
+                continue
+            end
+            if (standard_generators_x[i, j]==1) && (standard_generators_z[i, j]==1)
+                push!(encoding_circuit, Op("CX", i, j))
+                push!(encoding_circuit, Op("CZ", i, j))
+            elseif standard_generators_x[i, j]==1
+                push!(encoding_circuit, Op("CX", i, j))
+            elseif standard_generators_z[i, j]==1
+                push!(encoding_circuit, Op("CZ", i, j))
+            end
+        end
+    end
+
+    return encoding_circuit
+end
+
 
 """
     StabilizerCode(n::Int,k::Int,d::Int,stabilizers::Vector,logicals::Dict)
@@ -169,12 +214,11 @@ struct StabilizerCode #alpha version
     stabilizers::Vector
     generator::AbstractMatrix
     generator_standard::AbstractMatrix
-    logical_XZ_ops::Vector
     logicals::Dict
     codestates::Vector
     codewords::Vector
     stabilizer_matrix::Vector
-    stats::NamedTuple
+    info::NamedTuple
     ops::Function
     apply::Function
     encode::Function
@@ -183,20 +227,18 @@ struct StabilizerCode #alpha version
     correct::Function
     circuit::Function
 
-    function StabilizerCode(n::Int,k::Int,d::Int,stabilizers::Vector{String},logicals::Dict)
+    function StabilizerCode(stabilizers::Vector{String},logicals::Dict;d::Int=-1)
 
-        # if length(first(logicals)) != k
-            # throw("k and logicals are incompatible")
-        if length(split(stabilizers[1],","))!=n
-            throw("n and stabilizers are incompatible")
-        end
+        n=length(split.(stabilizers,",")[1])
+        m=length(stabilizers)
+        k=n-m
 
         ##find init from stabilizers
         stabilizer_matrix=string_to_matrix.(stabilizers)
         generator=stabilizers_to_generator(stabilizers)
-        generator_standard, logical_XZ_ops, XZ_vecs = standard_form_logicals(generator)
-        len_stabilizers=length(stabilizers)
-        e,v=la.eigen(Matrix(sum(stabilizer_matrix)/len_stabilizers))
+        generator_standard, logicals, logical_XZ_vecs, r = standard_form_logicals(generator, logicals)
+        encoding_ops=encoding_circuit_from_generator(generator_standard,logical_XZ_vecs,r)
+        e,v=la.eigen(Matrix(sum(stabilizer_matrix)/m))
         codestates = [sa.sparse(v[:, i]) for i in 1:length(e) if abs(e[i] - 1) < 1000eps()];sa.droptol!.(codestates,1000eps());
         len_codestates=length(codestates)
         codewords=[get_codeword(code_s) for code_s=codestates]
@@ -206,14 +248,31 @@ struct StabilizerCode #alpha version
 
         ##
 
-        function new_encode(state::sa.SparseVector)
+        function new_encode(state_init::sa.SparseVector)
 
-            throw("fix")
+            N=get_N(state_init)
+            if N!=k
+                throw(ArgumentError("init state cannot be larger than k=$(k) of the code."))
+            end
+
+            state=kron(zero_state(n-k),state_init)
+
+            for o=encoding_ops
+                state=apply(state,o)
+            end
+
+            return state
+
         end
 
         function new_decode(state::sa.SparseVector)
 
-            throw("fix")
+            for o=encoding_ops'
+                state=apply(state,o)
+            end
+
+            return sa.sparse(sa.findnz(state)[2])
+
         end
 
         function new_syndrome(state::sa.SparseVector)
@@ -229,6 +288,7 @@ struct StabilizerCode #alpha version
         function new_circuit(state::sa.SparseVector)
 
             throw("fix")
+
         end
 
         new_ops(name::String, logical_qubit::Int)=[Op(x...) for x=logicals[name,logical_qubit]]
@@ -290,14 +350,16 @@ struct StabilizerCode #alpha version
 
         new_apply(state::Union{sa.SparseVector,sa.SparseMatrixCSC}, op::Op; noise::Union{Bool,NoiseModel}=false)=op.q==1 ? new_apply(state, op.name, op.qubit;noise=noise) : new_apply(state, op.name, op.qubit, op.target_qubit;noise=noise)
         
-        stats=(
+        info=(
             len_logical=length(keys(logicals)),
-            len_stabilizers=len_stabilizers,
             len_codestates=len_codestates,
-            XZ_vecs=XZ_vecs
+            logical_XZ_vecs=logical_XZ_vecs,
+            encoding_ops=encoding_ops,
+            r=r,
+            m=m #len_stabilizers
         )
 
-        return new(n,k,d,stabilizers,generator,generator_standard,logical_XZ_ops,logicals,codestates,codewords,stabilizer_matrix,stats,new_ops,new_apply,new_encode,new_decode,new_syndrome,new_correct,new_circuit);
+        return new(n,k,d,stabilizers,generator,generator_standard,logicals,codestates,codewords,stabilizer_matrix,info,new_ops,new_apply,new_encode,new_decode,new_syndrome,new_correct,new_circuit);
 
     end
 
